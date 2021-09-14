d180ba851d408166088d824967ae2c968b96487f
tony <antolisah@gmail.com>
2021-09-15 02:02:27 +0300

    query options

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,46 @@
 const items = require('../items');
 
-function itemRoutes(fastify, options, done) {
+// Options for get all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string' },
+          },
+        }
+      }
+    }
+  }
+};
 
-  fastify.get('/items', async (req, reply) => {
+// Options for get a single item
+const getItemByIdOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          name: { type: 'string' },
+        }
+      }
+    }
+  }
+};
+
+function itemRoutes(fastify, options, done) {
+  // Get all items
+  fastify.get('/items', getItemsOpts, async (req, reply) => {
     reply.send(items);
   });
 
-  fastify.get('/items/:id', async (req, reply) => {
+  // Get a single item
+  fastify.get('/items/:id', getItemByIdOpts, async (req, reply) => {
     const id = req.params.id;
     const item = items.find(i => i.id === id);
     reply.send(item);
