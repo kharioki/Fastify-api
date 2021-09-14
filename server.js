3b4153dec81e23c073d704b58b1e06d6f908957f
tony <antolisah@gmail.com>
2021-09-15 00:21:25 +0300

    creating a basic route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const fastify = require('fastify')({ logger: true });
 const PORT = 5000;
 
+fastify.get('/items', async (req, res) => {
+  res.send({ test: 'Hello' });
+});
+
 const start = async () => {
   try {
     await fastify.listen(PORT);
